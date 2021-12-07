3cdba9c1ab422da788d6efc11fd8948c395f3b1a
Frank-Proulx <frankproulx7@gmail.com>
2021-12-06 16:30:51 -0800

    WIP: start logic for minutes/hours
    Co-authored-by: Marisa Edgar <marisaedgar1212@gmail.com>

---
+++ lib/clock_angle.rb
@@ -0,0 +1,8 @@
+require('pry')
+
+def clock_angle(hours, minutes)
+  hour_actual = (((hours * 60) + minutes) / 720) * 360
+  minutes_actual = (minutes / 60) * 360
+  
+
+end
--- spec/clock_angle_spec.rb
+++ spec/clock_angle_spec.rb
@@ -1,3 +1,17 @@
 require('rspec')
 require('clock_angle')
 
+describe('#clock_angle') do
+  it('convert hours into minute values') do
+    expect(clock_angle(4, 30)).to(eq())
+  end
+
+
+
+
+
+  # it('tell the amount of degrees between the hour and minute hand, giving the smallest possible value') do
+  #   expect(clock_angle(1,30)).to(eq(150))
+  # end
+
+end
